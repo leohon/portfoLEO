dcb92efc024407d6b99301e4594944d3031bd9b4
leohon <leo.cl.hon@gmail.com>
2022-11-28 18:44:59 -0500

    fixed up code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Alert for non-live projects
-const liveEl = document.querySelectorAll('#notLive')
-console.log(liveEl)
+const liveEl = document.querySelectorAll('#notLive');
 
 liveEl.forEach(function(button) {
   button.addEventListener('click', function() {
@@ -24,9 +23,10 @@ iconEl.forEach(function(icon) {
 const nameEl = document.querySelector('#name');
 const phoneEl = document.querySelector('#phone');
 const emailEl = document.querySelector('#email');
-const commentEl = document.querySelector('#comment');
+const messageEl = document.querySelector('#message');
 const buttonEl = document.querySelector('.formButton');
 const formEl = document.querySelector('form')
+console.log(nameEl)
 
 buttonEl.addEventListener('click', function(event) {
   event.preventDefault;
@@ -34,10 +34,13 @@ buttonEl.addEventListener('click', function(event) {
   const name = nameEl.value;
   const phone = phoneEl.value;
   const email = emailEl.value;
-  const comment = commentEl.value;
+  const message = messageEl.value;
 
-  if (name && phone && email && comment) {
+  if (name && phone && email && message) {
     formEl.innerHTML = `Thanks for reaching out!`
+    formEl.style.fontSize = "2rem";
+    formEl.style.paddingTop = "100px";
+    formEl.style.color = "gold";
   }
   else {
     if (!name) {
