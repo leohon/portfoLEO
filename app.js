992a4cf6d6b71771a3830a612848b8b868b0cd09
leohon <leo.cl.hon@gmail.com>
2023-01-05 16:22:11 -0500

    Made form send to email using Formspree

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Nav hamburger menu
 // SOURCE: https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
-const menu = document.querySelector(".menu");
-const hamburger = document.querySelector(".hamburgerMenu");
-const open = document.querySelector(".fa-solid.fa-bars");
-const close = document.querySelector(".fa-solid.fa-xmark");
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburgerMenu');
+const open = document.querySelector('.fa-solid.fa-bars');
+const close = document.querySelector('.fa-solid.fa-xmark');
 
 hamburger.addEventListener("click", function() {
   // Open menu
@@ -55,41 +55,9 @@ iconImgEl.forEach(function(icon) {
   });
 })
 
-// Form feedback upon submit
-const nameEl = document.querySelector('#name');
-const phoneEl = document.querySelector('#phone');
-const emailEl = document.querySelector('#email');
-const messageEl = document.querySelector('#message');
-const buttonEl = document.querySelector('.formButton');
-const formEl = document.querySelector('form');
 
-buttonEl.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  const name = nameEl.value;
-  const phone = phoneEl.value;
-  const email = emailEl.value;
-  const message = messageEl.value;
-
-  if (name && phone && email && message) {
-    formEl.innerHTML = `Thanks for reaching out!`;
-    formEl.style.fontSize = "2rem";
-    formEl.style.paddingTop = "100px";
-    formEl.style.color = "gold";
-  }
-  else {
-    if (!name) {
-      alert("Please fill in name.");
-    }
-    else if (!phone) {
-      alert("Please fill in phone number.");
-    }
-    else if (!email) {
-      alert("Please fill in email.");
-    }
-    else {
-      alert("Please write a message.");
-    }
-  }
-})
-
+// Reset form on page load after Formspree submit and 'Go back' clicked
+document.getElementById("name").value = "";
+document.getElementById("phone").value = "";
+document.getElementById("email").value = "";
+document.getElementById("message").value = "";
